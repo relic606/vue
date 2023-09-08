d824cce5fd52b3f91ffcf0e26595ac4fb79287a6
Andy <relic606@yahoo.com>
2023-09-08 09:52:55 -0600

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["testProp"]);
+
+const name = ref("Andy");
+
+function nameChange() {
+  name.value = "AndyButton";
+}
+</script>
+
+<style scoped>
+div {
+  background-color: yellow;
+}
+</style>
+
+<template>
+  <button @click="nameChange">Click to Change Name</button>
+  <div>Testing prop in child: {{ props.testProp }}</div>
+  <div>{{ name }}</div>
+</template>
---
+++ src/views/TestView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Test from "../components/Test.vue";
+import { ref } from "vue";
+const testProp = ref("Test Property");
+</script>
+
+<template>
+  <main>
+    <div>Testing Vue new page</div>
+    <Test v-bind:testProp="testProp" />
+    <div>Testing prop: {{ testProp }}</div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: lightblue;
+}
+</style>
